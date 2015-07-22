f7b19dc7181b2be0255a5b9102301ed6c08e6d2a
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-22 14:18:29 -0500

    Fixed edit user for whether they changed their password or not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,10 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)
-    
+    @user = User.new(user_params_and_password)
+    @user.encrypt_password(params[:user][:password])
     if @user.save
-      redirect_to login_path
+      redirect_to users_path
     else
       render "new"
     end
@@ -23,10 +23,22 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      redirect_to users_path
+    if params[:user][:password] != "" && BCrypt::Password.new(@user.password) != params[:user][:password]
+      @user.encrypt_password(params[:user][:password])
+      params[:user][:password] = @user.password
+      if @user.update_attributes(user_params_and_password)
+        binding.pry
+        redirect_to users_path
+      else
+        render "edit"
+      end
     else
-      render "edit"
+      if @user.update_attributes(user_params)
+        binding.pry
+        redirect_to users_path
+      else
+        render "edit"
+      end
     end
   end
   
@@ -43,6 +55,10 @@ class UsersController < ApplicationController
   private
   
   def user_params
+    params[:user].permit(:name, :email)
+  end
+  def user_params_and_password
     params[:user].permit(:name, :email, :password)
   end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
+  include BCrypt
+  
   has_many :items
   validates :name, presence: true
   validates :email, presence: true
+  
+  
+  def encrypt_password(unencrypted_password)
+    self.password = BCrypt::Password.create(unencrypted_password)
+  end
+    
 end
