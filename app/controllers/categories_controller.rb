9643659ca038ada7554a0d69f16b2ed2125b26dc
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-22 22:16:56 -0500

    Category#destroy

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -27,6 +27,15 @@ class CategoriesController < ApplicationController
       render "edit"
     end
   end
+  
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to user_path
+  end
+  
+  
+  
   private
   
   def category_params
