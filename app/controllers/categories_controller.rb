aa5cf1aa0e138bd7eae8ba4c1d5e514ca744df3c
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-22 22:06:51 -0500

    Category#edit specific to each user added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,6 +13,20 @@ class CategoriesController < ApplicationController
     @categories = User.find(session[:user_id]).categories
   end
   
+  def edit
+    @category = Category.find(params[:id])
+    @user = User.find(session[:user_id])
+  end
+  
+  def update
+    @category = Category.new(category_params)
+    
+    if @category.save
+      redirect_to user_path
+    else
+      render "edit"
+    end
+  end
   private
   
   def category_params
