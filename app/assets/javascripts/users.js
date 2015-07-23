0f6cc3f93360c90b9f5177e9e17cbac83ce64379
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-23 14:34:08 -0500

    Starting to try to add AJAX

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.onload = markCheckboxes;
+
+function markCheckboxes() {
+  $(".checkbox_for_item").onchange = markChecked;  
+}
+
+function markChecked() {
+  alert("hi")
+}
